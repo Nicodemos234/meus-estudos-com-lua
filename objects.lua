6bb5c81f978a54d99d1cbc4db230d84bf04e2ef4
Nicodemos234 <nicodemos54324@gmail.com>
2021-06-27 21:30:14 -0300

    objects

--- objects.lua
+++ objects.lua
@@ -2,7 +2,16 @@
 -- do cochete quanto diretamente o texto
 ultimaCopa = {
     ano = 2002,
-    ["sede"] = "Japão e Coreia do Sul"
+    ["sede"] = "Japão e Coreia do Sul",
+    -- Posso por objetos dentro de objetos
+    jogadores = {"Cafu", "Ronaldo"},
+    -- Posso adicionar função dentro de objetos e para
+    -- acessar valores do proprio objeto usar self
+    imprime = function(self)
+        for k, v in ipairs(self.jogadores) do
+            print(k, v)
+        end
+    end
 }
 
 -- Quando eu coloco a chave no mapa 
@@ -15,3 +24,17 @@ print(ultimaCopa.ano)
 -- Posso adicionar nova chave digitando .nome da chave
 ultimaCopa.capitao = "Cafu"
 print(ultimaCopa.capitao)
+
+-- table.insert para adicionar na tabela
+table.insert(ultimaCopa.jogadores, "Rivaldo")
+table.insert(ultimaCopa.jogadores, "Zico")
+
+-- table.remove para remover da tela com o indice
+table.remove(ultimaCopa.jogadores, 4)
+
+-- Precisa passar o objeto para imprimir
+ultimaCopa.imprime(ultimaCopa)
+
+-- Para não precisar passar posso usar os : que passar
+-- só a propria tabela
+ultimaCopa:imprime()
